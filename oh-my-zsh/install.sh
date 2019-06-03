103727a2fbb8e03d49fca88e4f6e56e2375fd88a
exMachina <shyampadia@live.com>
2019-06-03 08:12:35 -0700

    fixed git config and basic installation. close #1 and close #8

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -4,11 +4,12 @@ source ../helpers.sh
 
 # Download and install zsh and powerlevel9k and nerd fonts
 if which zsh > /dev/null; then
-    echo 'ZSH not found... Installing'
+    info 'ZSH not found... Installing'
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
     # Install powerlevel9k
     # Install zsh plugins
+    success 'ZSH setup finish'
 else
-    echo 'ZSH already installed... Skipping'
+    info 'ZSH already installed... Skipping'
 fi
 
