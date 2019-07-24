d41ee05519558a9dace4c78d2bc84a0883d65a72
Padia <shyampadia@live.com>
2019-07-24 09:18:30 -0700

    fixed zsh plugin installation and vim theme

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -13,30 +13,36 @@ else
     info 'ZSH already installed... Skipping'
 fi
 
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel9k" ]; then
+INSTALL_DIR="${ZSH_CUSTOM:=/Users/exmachina/.oh-my-zsh/custom}"
+
+if [ ! -d "${INSTALL_DIR}/themes/powerlevel9k" ]; then
     user 'Do you want to install Powerlevel9k? [y]es, [n]o'
     read -n 1 pl9
     if [ $pl9 == "y" ]; then
         # Install powerlevel9k
         info 'Installing Powerlevel9k'
-        git clone https://github.com/bhilburn/powerlevel9k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel9k
+        git clone https://github.com/bhilburn/powerlevel9k.git ${INSTALL_DIR}/themes/powerlevel9k
         success 'Powelevel9k has ben setup'
     fi
 fi
 
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel9k" ]; then
+if [ ! -d "${INSTALL_DIR}/plugins/zsh-autosuggestions" ]; then
     user 'Do you want to install (fish like) ZSH Plugins? [y]es, [n]o'
     read -n 1 plugins
     if [ $plugins == "y" ]; then
         # Install zsh plugins
         info 'Installing Zsh Plugins'
-        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-        git clone https://github.com/zsh-users/zsh-apple-touchbar ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-apple-touchbar
-        git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${INSTALL_DIR}/plugins/zsh-autosuggestions
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting ${INSTALL_DIR}/plugins/zsh-syntax-highlighting
+        git clone https://github.com/zsh-users/zsh-apple-touchbar ${INSTALL_DIR}/plugins/zsh-apple-touchbar
+        git clone https://github.com/zsh-users/zsh-history-substring-search ${INSTALL_DIR}/plugins/zsh-history-substring-search
         success 'ZSH plugins have been installed'
     fi
 fi
 
 # Make zsh default shell
 # chsh -s $(which zsh)
+
+# change git aliases
+# sed -i.backup 's/ggpush/gopush/;s/ggpull/gopull/;s/gst/gs/' ~/.oh-my-zsh/plugins/git/git.plugin.zsh
+
--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+source ./helpers.sh
 
 nvim_config_file=~/.config/nvim
 
@@ -8,3 +9,15 @@ if [ ! -L "$nvim_config_file" ]; then
   ln -s ~/.dotfiles/vim/vim.symlink "$nvim_config_file"
 fi
 
+# pip install neovim
+# npm install neovim
+
+# Install vim plugins
+# nvim +PlugInstall +qall > /dev/null
+
+# nvim typescript install
+# https://github.com/mhartington/nvim-typescript/issues/197#issuecomment-469134118
+
+# Fix nerdtree-git-plugin
+# https://github.com/vim-syntastic/syntastic/issues/1391#issuecomment-97310854
+# sed -i '' 's/call s:FileUpdate/nested call s:FileUpdate/' ~/.dotfiles/vim/vim.symlink/plugged/nerdtree-git-plugin/nerdtree_plugin/git_status.vim
